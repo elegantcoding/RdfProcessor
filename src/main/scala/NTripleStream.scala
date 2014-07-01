ee0f64b92c1bd9e7cdd0355c4c005131d3532fec
Wes Freeman <wes@skeweredrook.com>
2014-07-01 13:50:31 -0400

    adding more tests, renaming file.

--- src/main/scala/NTripleStream.scala
+++ src/main/scala/NTripleStream.scala
@@ -19,14 +19,19 @@ class NTripleStream(is:InputStream) extends RdfStream {
 
   // this is significantly faster (half an order of magnitude) than split
   def parseTriple(rdfLine: String):RdfTriple = {
+    if (rdfLine.trim().length() == 0)
+      return InvalidRdfTriple()
+
     val idx = rdfLine.indexOf('\t')
     if (idx <= 0)
-      return InvalidRdfTriple(rdfLine.substring(0, idx))
+      return InvalidRdfTriple(rdfLine.trim())
+
     val first = rdfLine.substring(0, idx)
     val idx2 = rdfLine.indexOf('\t', idx + 1)
     if (idx2 <= 0)
       return InvalidRdfTriple(first,
-        rdfLine.substring(idx + 1, idx2))
+        rdfLine.substring(idx + 1, rdfLine.length).trim())
+    
     val second = rdfLine.substring(idx + 1, idx2)
     val idx3 = rdfLine.lastIndexOf('\t')
     val third = rdfLine.substring(idx2 + 1, idx3)
