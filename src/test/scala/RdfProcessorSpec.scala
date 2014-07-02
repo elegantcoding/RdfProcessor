fd28a3616d2166896f625448c62ad62986e2eaae
Wes Freeman <wes@skeweredrook.com>
2014-07-02 13:39:15 -0400

    fixing build

--- src/test/scala/RdfProcessorSpec.scala
+++ src/test/scala/RdfProcessorSpec.scala
@@ -1,8 +1,4 @@
-import java.io.{StringReader, BufferedReader}
 import org.scalatest._
-import com.elegantcoding.rdfProcessor._
-import rdftriple.rdftriple.RdfTriple
-import rdftriple.ValidRdfTriple
 
 class RdfProcessorSpec extends FlatSpec with Matchers {
 
